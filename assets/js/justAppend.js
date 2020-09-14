952caa38d32c5db59571cdc6fd6beea8ac553bf3
sajdiur <sajdiur.inbox@gmai.com>
2020-09-14 23:17:18 +0600

    MainArray

--- assets/js/justAppend.js
+++ assets/js/justAppend.js
@@ -1,4 +1,62 @@
 
+var MainArray = [];
+
+function textFieldTake(){
+    
+    var lbl = document.getElementById("labelSelector").value; 
+    var nm = document.getElementById("nameSelector").value;
+    var plchldr = document.getElementById("placeholderSelector").value;
+
+    var textField = {
+        field: "textField",
+        label: lbl,
+        name: nm,
+        placeholder: plchldr
+    }
+   
+    MainArray.push(textField)
+    //var test = Object.values(MainArray[0]);
+
+    lbl = document.getElementById("labelSelector").value=''; 
+    nm = document.getElementById("nameSelector").value='';
+    plchldr = document.getElementById("placeholderSelector").value='';
+
+    // For show
+    textFieldShow(MainArray[0])
+}
+
+
+
+function textFieldShow(textFieldInfo){
+    
+    var labelText = textFieldInfo['label'];
+    var nameText = textFieldInfo['name'];
+    var placeText = textFieldInfo['placeholder'];
+
+    var lbl = document.createElement("LABEL");
+    var elm = document.createElement("INPUT");
+
+    var textNode = document.createTextNode(labelText);
+    lbl.appendChild(textNode)
+
+    elm.setAttribute('type','text');
+    elm.setAttribute('name',nameText);
+    elm.setAttribute('placeholder',placeText);
+
+    document.getElementById("dynamicForm").appendChild(lbl);
+    document.getElementById("dynamicForm").appendChild(elm);
+
+}
+
+
+
+
+
+
+
+// Raw code.... 
+
+
 function textField(){
 
     //create element
